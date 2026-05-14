1c500ac32b085b15da6f73b7672bff556ea8c193
rilley <rrtaberdo6627ant@student.fatima.edu.ph>
2026-05-14 19:10:59 +0800

    fix config.php

--- auth/config.php
+++ auth/config.php
@@ -10,16 +10,4 @@ $conn = new mysqli($host, $username, $password, $dbname, $port);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-?><?php
-$host = getenv('DB_HOST') ?: 'turntable.proxy.rlwy.net';
-$port = getenv('DB_PORT') ?: '41685';
-$dbname = getenv('DB_NAME') ?: 'railway';
-$username = getenv('DB_USER') ?: 'root';
-$password = getenv('DB_PASS') ?: 'itNBYATxTbQnJsCqApUXObUVFdEulyZH';
-
-$conn = new mysqli($host, $username, $password, $dbname, $port);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
 ?>
