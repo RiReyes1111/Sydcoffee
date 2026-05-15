8b19fd6dfb86d670b135167d662687c677fe4714
rilley <rrtaberdo6627ant@student.fatima.edu.ph>
2026-05-15 08:16:01 +0800

    time

--- menu.php
+++ menu.php
@@ -15,7 +15,7 @@
   $isAdmin = ($loggedIn && $userRole === 'admin');
 
   $currentHour = (int)date('H');
-  $isHappyHour = ($currentHour >= 9 && $currentHour < 11);
+  $isHappyHour = ($currentHour >= 8 && $currentHour < 11);
 
   $discount     = 0.10;
   $coldDiscount = ($isUser && $isHappyHour) ? 0.20 : ($isUser ? 0.10 : 0);
